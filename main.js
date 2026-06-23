c3f3e66530450a806b1557920b4f613d47d1b898
Fernando Asato <fp.asato.dev@gmail.com>
2026-06-23 20:28:35 -0300

    add logo icon to BrowserWindow and update main.js

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 const { app, BrowserWindow } = require("electron")
+const path = require("path")
 
 function criarJanela() {
     const janela = new BrowserWindow({
         width: 1200,
-        height: 800
+        height: 800,
+        icon: path.join(__dirname, "public/icons", "logo.png"),
     })
 
     janela.loadFile("public/login.html")
